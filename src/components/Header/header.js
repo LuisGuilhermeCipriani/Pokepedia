6ab751f051bd0a5496b8fa09b3386dc612f57a2f
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-03-29 18:50:24 -0300

    Estiliza cabeçalho

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./header.css";
+
 export default function Header(){
     return(
-        <header>
-            <Link to="/">Home</Link>
-            <Link to="/pokedex">Pokedéx</Link>
+        <header className="container">
+            <Link to="/" className="link">Home</Link>
+            <Link to="/pokedex" className="link">Pokédex</Link>
         </header>
     );
 }
